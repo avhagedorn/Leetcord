68decf69552fa6c1f0b475078e93db64124f59c4
Kanishk-K <kanishkkacholia@gmail.com>
2022-07-07 19:24:22 -0500

    Removed file import

--- Web/LeetcodeTracker/settings.py
+++ Web/LeetcodeTracker/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-with open(BASE_DIR.joinpath("config.json")) as f:
-    data = json.load(f)
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-a(&zzm2_c!^cb0e6oyt934vnf*$y^7j8ym_j8c35azt*zrqrai"
