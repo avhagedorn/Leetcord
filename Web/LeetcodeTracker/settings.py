ad4a0b05a1b765fbc83d741989bac964c06b861e
Kanishk-K <kanishkkacholia@gmail.com>
2022-07-08 17:43:19 -0500

    Updated Allowed Hosts

--- Web/LeetcodeTracker/settings.py
+++ Web/LeetcodeTracker/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY") or data['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://leetcode-discord.herokuapp.com/','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['leetcode-discord.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
