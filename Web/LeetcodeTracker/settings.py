1dfbd2d83ac07e6ce9743f5323989cd3b1fbf41e
Kanishk-K <kanishkkacholia@gmail.com>
2022-07-08 19:08:29 -0500

    Added django-heroku back to requirements to ensure proper deploy. Does not touch database, hosts, or secret key.

--- Web/LeetcodeTracker/settings.py
+++ Web/LeetcodeTracker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import json
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals(),databases=False,allowed_hosts=False,secret_key=False)
