7761c2bb324ca3c7c63fe0da6780de36fa1fda01
Kanishk-K <kanishkkacholia@gmail.com>
2022-07-08 21:16:57 -0500

    Created initial schema for web-app

---
+++ Web/Data/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    discordID = models.IntegerField()
+    discordName = models.CharField(max_length=35)
+    discordPFP = models.URLField()
+    date_verified = models.DateField(auto_now_add=True)
+
+class Problem(models.Model):
+    class DIFFICULTY_CHOICES(models.IntegerChoices):
+        EASY = 0, "Easy"
+        MEDIUM = 1, "Medium"
+        HARD = 2, "Hard"
+    problem_number = models.SmallIntegerField()
+    slug = models.CharField(max_length=200)
+    difficulty = models.SmallIntegerField(choices=DIFFICULTY_CHOICES.choices)
+
+class Solve(models.Model):
+    solvee = models.ForeignKey("Member",related_name="Solves",on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    problem = models.ForeignKey("Problem", related_name="Solves",on_delete=models.CASCADE)
+    takeaway = models.CharField(max_length=255)
