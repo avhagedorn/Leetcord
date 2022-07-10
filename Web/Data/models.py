a1435ba7d4dea6043b7749be5849b7dbee4caca8
Kanishk-K <kanishkkacholia@gmail.com>
2022-07-09 19:17:47 -0500

    Increment and Decrement total solves from database.

--- Web/Data/models.py
+++ Web/Data/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
 
 # Create your models here.
 def update_children(member):
@@ -8,6 +10,7 @@ class Member(models.Model):
     discordName = models.CharField(max_length=35)
     discordPFP = models.URLField()
     date_verified = models.DateField(auto_now_add=True)
+    num_solutions = models.SmallIntegerField(default=0)
 
     def __str__(self) -> str:
         return f"Member {self.discordName} verified on {self.date_verified}"
@@ -24,6 +27,7 @@ class Problem(models.Model):
     def __str__(self) -> str:
         return f"Leetcode {self.problem_number} : {self.slug} | {self.get_difficulty_display()}"
 
+
 class Solve(models.Model):
     solvee = models.ForeignKey("Member",related_name="Solves",on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
@@ -31,3 +35,16 @@ class Solve(models.Model):
     takeaway = models.CharField(max_length=255,null=True,blank=True)
     def __str__(self) -> str:
         return f"{self.solvee.discordName}'s solution to {self.problem} on {self.date}"
+
+    def save(self, *args, **kwargs) -> None:
+        self.solvee.num_solutions += 1
+        self.solvee.save()
+        return super().save(*args, **kwargs)
+
+
+@receiver(pre_delete, sender=Solve)
+def decrement_deleted_solve(sender,instance,**kwargs):
+    instance.solvee.num_solutions -= 1
+    instance.solvee.save()
+
+
