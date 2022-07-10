b088195ff0998a8e232ac36830ce1051770f1545
Kanishk-K <kanishkkacholia@gmail.com>
2022-07-10 00:41:06 -0500

    Added embed formatting for responses. Also replaced number display for problems/solutions to name.

--- Bot/main.py
+++ Bot/main.py
@@ -1,6 +1,7 @@
 import os
 import json
 from bot import LeetcodeBot
+from HelpCommand import LeetcordHelpCommand
 
 if os.path.exists(os.path.join(os.path.dirname(__file__),"config.json")):
     f = open(os.path.join(os.path.dirname(__file__),"config.json"))
@@ -10,14 +11,13 @@ if os.path.exists(os.path.join(os.path.dirname(__file__),"config.json")):
 def load(key):
     return os.getenv(key) or data[key]
 
-IS_TEST = os.getenv('DISCORD_TOKEN') == None
 TOKEN = load('DISCORD_TOKEN')
 
 lcb = LeetcodeBot()
+lcb.help_command = LeetcordHelpCommand()
 
 @lcb.event
 async def on_ready():
     print(f"{lcb.user} is online")
 
-if IS_TEST:
-    lcb.run(TOKEN)
+lcb.run(TOKEN)
